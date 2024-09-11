b4b105e6fe783a11d8108e07b3748d658f876963
henriqueSpencer <henriquespencer11@gmail.com>
2024-09-11 15:59:30 -0300

    🌵Curso III

--- Curse_Level_III.py
+++ Curse_Level_III.py
@@ -1,2 +1,23 @@
-def curse_level_III_tasks():
-    pass
+""" 
+ This is the third level of the curse
+ 
+ Problema a ser resolvido:
+    - Your robot will produce business data from raw input data and then consume that data in small chunks - work items.
+    - Split your robot code into multiple logical pieces
+    - Extract, Transform, Load - (ETL)
+    - Integrating the road traffic fatality rate API ---> insurance sales system
+    - Producer-consumer pattern (allows you to track the status of individual requests and mitigates the risk of everything falling apart)
+
+
+    -producer.py:
+    consumer.py
+
+┌──────────┐   ┌────────────┐   ┌──────────┐   ┌─────────────────┐
+│ Producer │ → │ Work items │ → │ Consumer │ → │ Done work items │ → 💰
+└──────────┘   └────────────┘   └──────────┘   └─────────────────┘
+                                      ↓                  ↑
+                       ┌───────────────────┐             ↑
+                       │ Failed work items │ → → Retry → ↑
+                       └───────────────────┘
+
+"""
